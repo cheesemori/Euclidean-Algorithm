301625dc53acb566661625ddd77d58b770fa1b16
cheesemori <2274159818@qq.com>
2025-01-16 14:51:04 +0000

    Add comments

--- GCD.py
+++ GCD.py
@@ -1,25 +1,36 @@
 def main():
+    """
+    Main function of the program. It includes input, input check
+    """
     a = int(input("Enter the first number: "))
     b = int(input("Enter the second number: "))
     if type(a) == int and type(b) == int:
         print(gcd(a, b))
-        again_check()
+        reuse_check()
     else:
         print("Invalid input, please try again.")
         main()
 
 def gcd(a,b):
-
+    """
+    Secondary function. It will check the bigger number and return the result
+    """
     if b > a:
         a,b=b,a
     return find_gcd(a,b)
 
 def find_gcd(a,b):
+    """
+    The algorithm of the program.
+    """
     if a == 0 or b == 0:
         return f"The GCD is {a+b}"
     return find_gcd(b,a % b)
 
-def again_check():
+def reuse_check():
+    """
+    This function will check if user want to use again or not.
+    """
     if input("Would you like to try again? Enter 'y' to try again. ") == "y":
         main()
     else:
